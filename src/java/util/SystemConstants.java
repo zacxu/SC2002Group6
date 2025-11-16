1ba2656576114502067a871f4aa9a10e4101b186
zacxu <williamxu0510@gmail.com>
2025-11-16 15:08:15 +0800

    added constants class

---
+++ src/java/util/SystemConstants.java
@@ -0,0 +1,28 @@
+package util;
+
+    
+/**
+ * SystemConstants class contains constants
+ * 
+ *  max number of applications per student, 
+ *  max number of internships per representative,
+ *  max number of slots per internship, 
+ *  max year of study for the basic level.
+ * 
+ * 
+ * 
+ */
+
+
+
+public final class SystemConstants {
+    public static final int MAX_APPLICATIONS_PER_STUDENT = 3;
+    public static final int MAX_INTERNSHIPS_PER_REP = 5;
+    public static final int MAX_SLOTS_PER_INTERNSHIP = 10;
+    public static final int BASIC_LEVEL_MAX_YEAR = 2;
+
+    private SystemConstants() {
+    }
+}
+
+
