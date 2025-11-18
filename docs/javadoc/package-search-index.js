7c906c882f97fdd98217bd6f97aaf748f2f30545
zacxu <williamxu0510@gmail.com>
2025-11-18 22:12:26 +0800

    added javadocs

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"boundary"},{"l":"controller"},{"l":"entity"},{"l":"repository"},{"l":"service"},{"l":"util"},{"l":"validator"}];updateSearchResults();
